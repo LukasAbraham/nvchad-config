2e6606e5c9020528bea3f27e1d3195a9f4f919a9
Tran Xuan Thanh <thanhthcsab@gmail.com>
2024-07-17 00:30:18 +0700

    feat: update LSP and plugin configurations

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,38 +1,62 @@
 return {
-  {
-    "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
-    config = function()
-      require "configs.conform"
-    end,
-  },
+    {
+        "stevearc/conform.nvim",
+        -- event = 'BufWritePre', -- uncomment for format on save
+        config = function()
+            require "configs.conform"
+        end,
+    },
 
-  -- These are some examples, uncomment them if you want to see them work!
-  -- {
-  --   "neovim/nvim-lspconfig",
-  --   config = function()
-  --     require("nvchad.configs.lspconfig").defaults()
-  --     require "configs.lspconfig"
-  --   end,
-  -- },
-  --
-  -- {
-  -- 	"williamboman/mason.nvim",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"lua-language-server", "stylua",
-  -- 			"html-lsp", "css-lsp" , "prettier"
-  -- 		},
-  -- 	},
-  -- },
-  --
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+    -- These are some examples, uncomment them if you want to see them work!
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            require("nvchad.configs.lspconfig").defaults()
+            require "configs.lspconfig"
+        end,
+    },
+
+    {
+        "williamboman/mason.nvim",
+        opts = {
+            ensure_installed = {
+                "lua-language-server",        -- Lua
+                "stylua",                     -- Lua formatter
+                "html-lsp",                   -- HTML
+                "css-lsp",                    -- CSS
+                "prettier",                   -- Prettier
+                "typescript-language-server", -- TypeScript and JavaScript
+                "pyright",                    -- Python
+                "omnisharp",                  -- C#
+                "clangd",                     -- C, C++
+                "json-lsp",                   -- JSON
+                "yaml-language-server",       -- YAML
+                "marksman",                   -- Markdown
+            },
+        },
+    },
+
+    {
+        "nvim-treesitter/nvim-treesitter",
+        opts = {
+            ensure_installed = {
+                "vim", "lua", "vimdoc",
+                "html", "css",
+                "javascript", "typescript",
+                "c", "cpp",
+                "c_sharp",
+                "python", "htmldjango",
+                "markdown",
+            },
+        },
+    },
+
+    {
+        "nvim-tree/nvim-tree.lua",
+        opts = {
+            view = {
+                adaptive_size = true,
+            },
+        },
+    },
 }
