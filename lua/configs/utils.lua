66d276378027e6120cac64a7186e08c81ecc80cf
Tran Xuan Thanh <thanhthcsab@gmail.com>
2024-07-22 01:11:47 +0700

    feat(mappings): add a key mapping to toggle transparency

---
+++ lua/configs/utils.lua
@@ -0,0 +1,7 @@
+local M = {}
+
+function M.toggle_transparency()
+    require("base46").toggle_transparency()
+end
+
+return M
--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,10 +1,12 @@
 require "nvchad.mappings"
 
 -- add yours here
+local utils = require("configs.utils")
 
 local map = vim.keymap.set
 
 map("i", "jk", "<ESC>")
+map("n", "<leader>tt", utils.toggle_transparency)
 
 -- map("n", ";", ":", { desc = "CMD enter command mode" })
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
