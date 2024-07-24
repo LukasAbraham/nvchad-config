afeb10439592d719ec07ab8d30dc9572747730ee
Tran Xuan Thanh <thanhthcsab@gmail.com>
2024-07-24 23:59:11 +0700

    feat(mappings): map key for closing all buffers

--- lua/configs/utils.lua
+++ lua/configs/utils.lua
@@ -4,4 +4,12 @@ function M.toggle_transparency()
     require("base46").toggle_transparency()
 end
 
+function M.toggle_theme()
+    require("base46").toggle_theme()
+end
+
+function M.close_all_buffers()
+    require("nvchad.tabufline").closeAllBufs()
+end
+
 return M
--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,16 +1,17 @@
 require "nvchad.mappings"
 
--- add yours here
 local utils = require("configs.utils")
 local map = vim.keymap.set
 
 map("i", "jk", "<ESC>")
+map("i", "kj", "<ESC>")
 
-map("n", "<C-d>", "<C-d>zz")
-map("n", "<C-u>", "<C-u>zz")
-map("n", "<leader>tt", utils.toggle_transparency)
+map("n", "<C-d>", "<C-d>zz", { desc = "Scroll half page down" })
+map("n", "<C-u>", "<C-u>zz", { desc = "Scroll half page up" })
+map("n", "<leader>tt", utils.toggle_transparency, { desc = "Toggle transparency" })
+map("n", "<leader>qa", utils.close_all_buffers, { desc = "Close all buffers" })
 
-map("v", "<M-j>", ":m '>+1<CR>gv=gv")
-map("v", "<M-k>", ":m '<-2<CR>gv=gv")
+map("v", "<M-j>", ":m '>+1<CR>gv=gv", { desc = "Move lines down" })
+map("v", "<M-k>", ":m '<-2<CR>gv=gv", { desc = "Move lines up" })
 -- map("n", ";", ":", { desc = "CMD enter command mode" })
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
