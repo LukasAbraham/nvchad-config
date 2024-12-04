4bfbfb48bfef077b0321f483d2d6ab7c53c72ab2
Tran Xuan Thanh <thanhthcsab@gmail.com>
2024-12-04 16:51:24 +0700

    update keymaps

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,9 +1,11 @@
 require "nvchad.mappings"
 
 local map = vim.keymap.set
+local nomap = vim.keymap.del
 
 -- Insert mode keymaps
-map("i", "jk", "<ESC>")
+map("i", "jk", "<ESC>", { desc = "escape insert mode (1)" })
+map("i", "<C-c>", "<ESC>", { desc = "escape insert mode (2)" })
 map("i", "<C-t>", function()
     require("cmp").complete()
 end, { desc = "trigger code suggestions" })
@@ -11,8 +13,9 @@ end, { desc = "trigger code suggestions" })
 
 -- Normal mode keymaps
 -- map("n", ";", ":", { desc = "CMD enter command mode" })
-map("n", "<C-d>", "<C-d>zz", { desc = "Scroll half page down" })
-map("n", "<C-u>", "<C-u>zz", { desc = "Scroll half page up" })
+map("n", "<C-d>", "<C-d>zz", { desc = "scroll half page down" })
+map("n", "<C-u>", "<C-u>zz", { desc = "scroll half page up" })
+map("n", "<C-a>", "<cmd>%y+<CR>", { desc = "general copy whole file" })
 
 map("n", "<leader>tt", function()
     require("base46").toggle_transparency()
@@ -30,3 +33,7 @@ end, { desc = "close all buffers" })
 -- Visual mode keymaps
 map("v", "<M-j>", ":m '>+1<CR>gv=gv", { desc = "Move lines down" })
 map("v", "<M-k>", ":m '<-2<CR>gv=gv", { desc = "Move lines up" })
+
+
+-- Remove default keymaps
+nomap("n", "<C-c>")
